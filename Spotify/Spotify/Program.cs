ab38cd5ee8048ea7ad114712613ca1c5c2de246c
jjanssen2306 <120579336+jjanssen2306@users.noreply.github.com>
2024-06-13 13:00:35 +0200

    begin klaargezet

--- Spotify/Spotify/Program.cs
+++ Spotify/Spotify/Program.cs
@@ -1,35 +1,58 @@
-﻿using System.Globalization;namespace Spotify{
+﻿using System;
+
+namespace Spotivy
+{
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello and welcome to Spotify");
-            Console.WriteLine("choose 1 for songs");
-            Console.WriteLine("choose 2 for playlists");
-            Console.WriteLine("choose 3 for albums");
-            Console.WriteLine("choose 4 for friends");
-            Console.WriteLine("choose 5 for artists");
+            Console.WriteLine("Hello and welcome to Spotivy");
+            Console.WriteLine("Choose 1 for songs");
+            Console.WriteLine("Choose 2 for playlists");
+            Console.WriteLine("Choose 3 for albums");
+            Console.WriteLine("Choose 4 for friends");
+            Console.WriteLine("Choose 5 for artists");
             string choseWhatToDo = Console.ReadLine();
 
             switch (choseWhatToDo)
             {
                 case "1":
-                    /* play song, look at song, select song */
+                    while (true) { 
+                    Song.CreateNewSong();
+                        Console.WriteLine("do you want to see all the songs y/n");
+                        string seeAllSongs = Console.ReadLine();
+                        if (seeAllSongs.ToLower() == "y")
+                        {
+                           Song.DisplayAllSongs();
+                        }
+                        Console.WriteLine("do you want to search all songsy/n");
+                        String searchForSong = Console.ReadLine();
+                        if (searchForSong.ToLower() == "y")
+                        {
+                        Console.WriteLine("Tiepe 1 to cearch for title ");
+                        Console.WriteLine("Tiepe 1 to cearch for  artist");
+                        Console.WriteLine("Tiepe 1 to cearch for  genre");
+                        
+
+
+                        }
+                    }
+
                     break;
                 case "2":
-                    /* add song, add album, new playlist, delete song, check playlist*/
+                    /* add song, add album, new playlist, delete song, check playlist */
                     break;
                 case "3":
                     /* play album, look at album, select album */
                     break;
                 case "4":
-                    // add friend, remove friend, check friend, check friend's playlist, copy friend's playlist
+                    /* add friend, remove friend, check friend, check friend's playlist, copy friend's playlist */
                     break;
                 case "5":
-                    /* check artist, check artist's songs, check artist's albums*/
+                    /* check artist, check artist's songs, check artist's albums */
                     break;
                 default:
-                    /*invaled answer*/
+                    /* invalid answer */
                     break;
             }
         }
---
+++ Spotify/Spotify/song.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Spotivy
+{
+    public class Song
+    {
+        public string Title { get; set; }
+        public string Artists { get; set; }
+        public string Genre { get; set; }
+
+
+        private static List<Song> allSongs = new List<Song>();
+
+        public Song(string title, string artists, string genre)
+        {
+            Title = title;
+            Artists = artists;
+            Genre = genre;
+        }
+
+        public static void CreateNewSong()
+        {
+            Console.WriteLine("Enter song title:");
+            string title = Console.ReadLine();
+
+            Console.WriteLine("Enter song artists:");
+            string artists = Console.ReadLine();
+
+            Console.WriteLine("Enter genre:");
+            string genre = Console.ReadLine();
+
+            Song newSong = new Song(title, artists, genre);
+            allSongs.Add(newSong);
+
+            Console.WriteLine("New song created:");
+            Console.WriteLine($"Title: {title}, Artists: {artists}, Genre: {genre}");
+        }
+
+        public static void DisplayAllSongs()
+        {
+            Console.WriteLine("All Songs:");
+            foreach (var song in allSongs)
+            {
+                Console.WriteLine($"Title: {song.Title}, Artists: {song.Artists}, Genre: {song.Genre}");
+            }
+        }
+    }
+}
